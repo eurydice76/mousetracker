b8a4b66a19f161e7f315333ae2550a65c05de4cd
Eric Pellegrini <pellegrini@ill.fr>
2021-04-19 14:04:01 +0200

    Improved previous commit

--- src/mousetracker/kernel/models/mouse_monitoring_model.py
+++ src/mousetracker/kernel/models/mouse_monitoring_model.py
@@ -44,23 +44,22 @@ class MouseMonitoringModel(PandasDataModel):
             n_properties = self._data_frame.n_properties
             n_header_properties = self._data_frame.n_header_properties
 
-            if col < n_header_properties:
-                return None
+            if col >= n_header_properties:
 
-            day = (col - n_header_properties)//n_properties
-            weight_column = n_header_properties + day*n_properties
+                day = (col - n_header_properties)//n_properties
+                weight_column = n_header_properties + day*n_properties
 
-            initial_weight = df.iloc[0, n_header_properties]
-            current_weight = df.iloc[0, weight_column]
+                initial_weight = df.iloc[0, n_header_properties]
+                current_weight = df.iloc[0, weight_column]
 
-            ratio = (initial_weight - current_weight)/initial_weight
-            # If there is a weight loss of more than 10% color the cell in red
-            if ratio >= 0.2:
-                return QtGui.QBrush(QtGui.QColor(100, 0, 0))
-            elif ratio >= 0.15 and ratio < 0.2:
-                return QtGui.QBrush(QtGui.QColor(150, 0, 0))
-            elif ratio >= 0.05 and ratio < 0.15:
-                return QtGui.QBrush(QtGui.QColor(200, 0, 0))
+                ratio = (initial_weight - current_weight)/initial_weight
+                # If there is a weight loss of more than 10% color the cell in red
+                if ratio >= 0.2:
+                    return QtGui.QBrush(QtGui.QColor(100, 0, 0))
+                elif ratio >= 0.15 and ratio < 0.2:
+                    return QtGui.QBrush(QtGui.QColor(150, 0, 0))
+                elif ratio >= 0.05 and ratio < 0.15:
+                    return QtGui.QBrush(QtGui.QColor(200, 0, 0))
 
             try:
                 return QtGui.QBrush(QtGui.QColor(255, 119, 51)) if np.isnan(float(value)) else QtGui.QBrush(QtCore.Qt.white)
